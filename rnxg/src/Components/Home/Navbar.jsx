655cf69b9b1e0e7484098ce0037dfa3cf14ada1e
adwait12345 <89302800+adwait12345@users.noreply.github.com>
2022-02-07 15:50:15 +0530

    2

--- rnxg/src/Components/Home/Navbar.jsx
+++ rnxg/src/Components/Home/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './navbar.css';
 export default function Navbar() {
-  return <div></div>;
+  return <div className='Navbar-wrapper'>
+
+This is a Navbar
+  </div>;
 }
