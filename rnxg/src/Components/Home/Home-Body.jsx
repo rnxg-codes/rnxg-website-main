1a46cbd2cde0582dd3f768f47ca84539ee2e6efd
adwait12345 <89302800+adwait12345@users.noreply.github.com>
2022-02-07 14:28:55 +0530

    1

---
+++ rnxg/src/Components/Home/Home-Body.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Home-Body.css';
+export default function HomeBody() {
+  return <div></div>;
+}
---
+++ rnxg/src/Components/Home/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './navbar.css';
+export default function Navbar() {
+  return <div></div>;
+}
