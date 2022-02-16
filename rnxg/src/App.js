58404bde530bc7ddc9062d7b58a3471a4a6e5544
K <karaleash462@gmail.com>
2022-02-16 19:28:33 +0530

    np

--- rnxg/src/App.js
+++ rnxg/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Home/Navbar';
+import Footer from './Components/Home/Footer';
 
 
 function App() {
   return (
 <>
-<Navbar />
 
+<Footer />
 </>
   );
 }
