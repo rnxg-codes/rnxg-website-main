6a276a94e1d7b56d5ceaa80e08b55ff80f975370
yashkondewar <kondewaryash@gmail.com>
2022-02-18 22:39:17 +0530

    commit

--- rnxg/src/App.js
+++ rnxg/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
 import Topbox from './Components/Home/topbox';
 
+=======
+import Anime from './Components/Pages/anime';
+import './Components/Pages/anime.css';
+>>>>>>> Stashed changes
 
 function App() {
   return (
 <>
+<<<<<<< Updated upstream
 <Topbox/>
+=======
+<Anime />
+
+>>>>>>> Stashed changes
 </>
   );
 }
--- rnxg/src/Components/Pages/anime.jsx
+++ rnxg/src/Components/Pages/anime.jsx
@@ -1,6 +1,82 @@
-import React from 'react';
-import './anime.css';
+import React from "react";
+import "./anime.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 export default function Anime() {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <div className="container" data-aos="zoom-in">
+          <div
+            className="card"
+            data-tilt
+            data-tilt-max={50}
+            data-tilt-speed={200}
+            data-tilt-perspective={700}
+            data-tilt-glare="true"
+            data-tilt-max-glare="0.8"
+          >
+            <div className="content">
+              <h2>01</h2>
+              <h3>Card One</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consectetur amet exercitationem molestias quae illum atque
+                consequuntur omnis eligendi mollitia asperiores consequatur
+                culpa beatae dolorum veritatis qui saepe, sint voluptas commodi?
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div
+            className="card"
+            data-tilt
+            data-tilt-max={50}
+            data-tilt-speed={200}
+            data-tilt-perspective={700}
+            data-tilt-glare="true"
+            data-tilt-max-glare="0.8"
+          >
+            <div className="content">
+              <h2>02</h2>
+              <h3>Card Two</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consectetur amet exercitationem molestias quae illum atque
+                consequuntur omnis eligendi mollitia asperiores consequatur
+                culpa beatae dolorum veritatis qui saepe, sint voluptas commodi?
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div
+            className="card"
+            data-tilt
+            data-tilt-max={50}
+            data-tilt-speed={200}
+            data-tilt-perspective={700}
+            data-tilt-glare="true"
+            data-tilt-max-glare="0.8"
+          >
+            <div className="content">
+              <h2>03</h2>
+              <h3>Card Three</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Consectetur amet exercitationem molestias quae illum atque
+                consequuntur omnis eligendi mollitia asperiores consequatur
+                culpa beatae dolorum veritatis qui saepe, sint voluptas commodi?
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>;
+AOS.init({
+  duration: 1000,
+});
