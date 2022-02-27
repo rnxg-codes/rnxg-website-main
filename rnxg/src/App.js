2ba8295305606ceeabc2e3bb2e8c796865f874a3
K <karaleash462@gmail.com>
2022-02-27 22:19:46 +0530

    kk

--- rnxg/src/App.js
+++ rnxg/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Home/Navbar';
 import Footer from './Components/Home/Footer';
+import Topbox from './Components/Home/topbox';
 
 function App() {
   return (
     <>
-
+      <Navbar />
+      <Topbox />
       <Footer />
     </>
   );
