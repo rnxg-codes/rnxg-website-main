c4adbde35fa4185d7e51db13fb0a92f4fcdb196a
adwait12345 <89302800+adwait12345@users.noreply.github.com>
2022-04-23 22:12:54 +0530

    today

--- rnxg/src/Components/Pages/Modal.jsx
+++ rnxg/src/Components/Pages/Modal.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import './modal.css'
 
-export default function Modal({show,setShow, project}) {
+export default function Modal({show,setShow, project,key}) {
   const handlePassInfoShow = (data)=>{
     // console.log(data.id);
 
   }
- const Updated=()=>{
-console.log(project.id-1)
- }
+
   return (
     <>
     {show ? <div key={project.id} className="modal-container" onClick={()=>handlePassInfoShow(project)} show={show}>
         
-        <div className="left-button">
-<button onClick={Updated()}><svg role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24"><path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z"></path></svg></button>
-        </div>
+        {/* <div className="left-button">
+<button onClick={()=>project+1} ><svg role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24"><path d="M15.957 2.793a1 1 0 010 1.414L8.164 12l7.793 7.793a1 1 0 11-1.414 1.414L5.336 12l9.207-9.207a1 1 0 011.414 0z"></path></svg></button>
+        </div> */}
         <div className="main-container-modal">
 
        
@@ -23,7 +21,7 @@ console.log(project.id-1)
                         <img src={project.img} alt="" />
 
          </div>
-         <div className="modal-right">
+         <div className="modal-right" >
            <div className="title-modal">
                          <h2>{project['Project-Name']}
   
@@ -34,13 +32,14 @@ console.log(project.id-1)
            </div>
 
              <hr />
-             <p>{project.Description}</p>
-
+             <p >{project.Description}</p>
+              <p><strong>{project.Team}</strong></p>
          </div>
  
-          </div>        <div className="right-button">
-<button> <svg role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24"><path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z"></path></svg></button>
-         </div>
+          </div>   
+               {/* <div className="right-button">
+<button onClick={()=>project.id+1}> <svg role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 jgfuCe IYDlXmBmmUKHveMzIPCF" viewBox="0 0 24 24"><path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z"></path></svg></button>
+         </div> */}
    </div>  
      : null}
 
--- rnxg/src/Components/Pages/Project.jsx
+++ rnxg/src/Components/Pages/Project.jsx
@@ -24,15 +24,24 @@ const openModal = ()=>{
   
 
 }
+  const body = document.querySelector(".Projects");
+//  if(setShow(true)){
+//     body.style.overflow = "hidden";
+//  }
+//  else if(setShow(false)){
+//   body.style.overflow = "auto";
 
+//  }c
+// {setShow(true)? document.getElementsByClassName('Projects').style = {overflow:"hidden"}: document.getElementsByClassName('Projects').style={overflow:"auto"}}
 const handlePassInfoShow = (data)=>{
   // setShow(true);
   setProject(data)
   console.log(data);
   setShow(true)
- }
 
+ 
 
+}
   return (
    <>
  <div className="Projects" id='top1'>
@@ -50,7 +59,7 @@ const handlePassInfoShow = (data)=>{
     offset={-70}
     duration={500}
 >
-<button>Explore <span><svg xmlns="http://www.w3.org/2000/svg" class="octicon arrow-symbol-mktg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path><path class="octicon-chevrow-stem" stroke="currentColor" d="M1.75 8H11" stroke-width="1.5" stroke-linecap="round"></path></svg></span></button>
+<button>Explore <span><svg xmlns="http://www.w3.org/2000/svg" className="octicon arrow-symbol-mktg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path><path className="octicon-chevrow-stem" stroke="currentColor" d="M1.75 8H11" strokeWidth="1.5" strokeLinecap="round"></path></svg></span></button>
       </Link>  
          </div>
          <div className="project-top-right"></div>
@@ -88,7 +97,7 @@ const handlePassInfoShow = (data)=>{
      </div>
      <div className="Project-bottom">
     
-     <Modal project={proj} show={show} setShow={setShow} 
+     <Modal key={project.id} project={proj} show={show} setShow={setShow} 
 
 />
 
