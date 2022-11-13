d43e30c3b4139ee3d152f063c783902b2e99fbbc
atharva_gitlab_030 <atharvas_jagdale@auton.in>
2022-11-13 23:58:17 +0530

    trial

--- rnxg/src/App.js
+++ rnxg/src/App.js
@@ -1,33 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeBody from "./Components/Home/Home-Body";
+import Profiles from "./Components/Home/profiles";
+import Events from "./Components/Pages/Events.jsx";
+import Project from "./Components/Pages/Project";
+import Terms from "./Components/Pages/terms";
+import Privicy from "./Components/Pages/Privicy";
 
-
-
-
-
-import logo from './logo.svg';
-import './App.css';
-import {useState,useEffect} from 'react'
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import HomeBody from './Components/Home/Home-Body';
-import Profiles from './Components/Home/profiles';
-import Events from './Components/Pages/Events.jsx';
-import Project from './Components/Pages/Project';
-import Terms from './Components/Pages/terms';
-import Privicy from './Components/Pages/Privicy';
-
-
-
-
-
-import Loader from './Components/Pages/loader';
-import ScrollToTop from './Components/Home/ScrolltoTop';
-import Registation from './Components/Pages/Registation';
-import Celebration from './Components/Home/celebration';
+import Loader from "./Components/Pages/loader";
+import ScrollToTop from "./Components/Home/ScrolltoTop";
+import Registation from "./Components/Pages/Registation";
+import Celebration from "./Components/Home/celebration";
 import { useLocation } from "react-router-dom";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 
-const TRACKING_ID = "UA-228810443-1" ; // OUR_TRACKING_ID
+const TRACKING_ID = "UA-228810443-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
-
 
 function App() {
   //
@@ -37,34 +27,56 @@ function App() {
   }, [location]);
   //
   const [loading, setLoading] = useState(false);
-  useEffect(()=>{
-setLoading(true)
-setTimeout(()=>{
-  setLoading(false)
-},1500)
-  },[])
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
 
   return (
-<>  <ScrollToTop/>
-
-<Routes>
-        <Route path="/" element={loading ? <Loader loading={loading}/>:<HomeBody/>}   />
-        <Route path="/profiles" element={ loading ? <Loader loading={loading}/>:<Profiles/> } />
-        <Route path="/events" element={ loading ? <Loader loading={loading}/>:<Events/> } />
-        <Route path="/projects" element={ loading ? <Loader loading={loading}/>:<Project/> } />
-        <Route path="/Terms" element={ loading ? <Loader loading={loading}/>:<Terms/> } />
-        <Route path="/Privicy" element={ loading ? <Loader loading={loading}/>:<Privicy/> } />
-        <Route path="/cele" element={ loading ? <Loader loading={loading}/>:<Celebration/> } />
-        <Route path="/registations" element={ loading ? <Registation loading={loading}/>:<Registation/> } />
-       
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path="/"
+          element={loading ? <Loader loading={loading} /> : <HomeBody />}
+        />
+        <Route
+          path="/profiles"
+          element={loading ? <Loader loading={loading} /> : <Profiles />}
+        />
+        <Route
+          path="/events"
+          element={loading ? <Loader loading={loading} /> : <Events />}
+        />
+        <Route
+          path="/projects"
+          element={loading ? <Loader loading={loading} /> : <Project />}
+        />
+        <Route
+          path="/Terms"
+          element={loading ? <Loader loading={loading} /> : <Terms />}
+        />
+        <Route
+          path="/Privicy"
+          element={loading ? <Loader loading={loading} /> : <Privicy />}
+        />
+        <Route
+          path="/cele"
+          element={loading ? <Loader loading={loading} /> : <Celebration />}
+        />
+        <Route
+          path="/registations"
+          element={
+            loading ? <Registation loading={loading} /> : <Registation />
+          }
+        />
       </Routes>
-     
-{/* <Profiles/> */}
-
-</>
+      {/* <Profiles/> */}
+    </>
   );
 }
 
 export default App;
 // var loader = document.getElementById("preloader");
-
