ba8c372b07bd6b96467dfd3f2785ffbb60b5644a
sarthak4399 <sarthakkhandare21@gmail.com>
2023-05-01 16:00:24 +0530

    + Confetti

--- rnxg/src/Components/Home/celebration.jsx
+++ rnxg/src/Components/Home/celebration.jsx
@@ -7,25 +7,24 @@ export default function Celebration () {
         document.getElementById('navbar12').style.top='0px';
         document.getElementById('news').style.display='none';
         document.getElementById('sprinkle').style.display='none';
-        
-        
           }
   return (
 
     <> 
     <div className="news" id='news'>
- 
+ <Confetti/>
   <div className="news-in">
   <h4>
-  <center>RNXG shortlisted for DD Robocon 2023 Stage-02 </center>
+  <center>RNXG shortlisted for DD Robocon 2023 Stage-03 </center>
    </h4>
    <div className="news-cancel">
-            <a href={require("../Home/PDF/Result of Stage 1 - DD-Robocon 2023.pdf")} download="Result of Stage 01"><button id='Know'>Know More</button></a>
+            <a href={require("../Home/PDF/Result of DD-Robocon 2023 Stage-2 Final (1).pdf")} download="Result of Stage 02"><button id='Know'>Know More</button></a>
 
      <button id='close' onClick={Cross}>
      <svg viewBox="0 0 24 24" width="24" height="24" class=""><path fill="#fff" d="m19.8 5.8-1.6-1.6-6.2 6.2-6.2-6.2-1.6 1.6 6.2 6.2-6.2 6.2 1.6 1.6 6.2-6.2 6.2 6.2 1.6-1.6-6.2-6.2 6.2-6.2z"></path></svg>
      </button>
-   </div></div>
+   </div>
+   </div>
 </div>
      </>
   )
